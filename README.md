9369edc5427621a07eb7652fdd0c00b5f6add11c
Jisse Reitsma <info@yireo.com>
2020-03-10 15:14:13 +0100

    Enhance look & feel

--- README.md
+++ README.md
@@ -41,21 +41,15 @@ An extension is able to offer unit tests. The following checks are in place for 
 
 - See whether a folder `Test/Unit` exists.
 
-@todo: Add additional tests when tests are located elsewhere.
-
 #### Integration tests
 An extension is able to offer integration tests. The following checks are in place for this:
 
 - See whether a folder `Test/Integration` exists.
 
-@todo: Add additional tests when tests are located elsewhere.
-
 #### MFTF tests
 An extension is able to offer MFTF tests. The following checks are in place for this:
 
 - See whether a folder `Test/Mftf` exists.
-
-@todo: Add additional tests when tests are located elsewhere.
 
 ## Development
 This repository ships with various tools to generate badgets. First of all, the `extensions/` folder holds JSON files, each file representing a badge for a specific extension. To generate a new JSON file, clone this repository and run `composer install`. Next, you can use the CLI tool `scripts/extdn_badge_information_generator.php` to point it to an extension folder:
--- js/badge.js
+++ js/badge.js
@@ -1,33 +1,55 @@
 var getExtDNBadge = function (extensionId, badgeCallback) {
     getExtDNBadgeInformation(extensionId, function (status, badgeInformation) {
-        function getYesOrNo(value) {
-            return (value === true ? '<span class="yes">Yes</span>' : '<span class="no">No</span>');
-        }
-
         console.log('ExtDN badge information', badgeInformation);
         var badgeHtml = '<div class="extdn-extension-badge">'
-            + '<div class="image">'
-            + '<picture>'
-            + '<source srcset="' + badgeInformation.image_webp + '" type="image/webp">'
-            + '<source srcset="' + badgeInformation.image_png + '" type="image/png">'
-            + '<img alt="' + badgeInformation.module_name + '" title="' + badgeInformation.module_name + '" src="' + badgeInformation.image_png + '">'
-            + '</picture>'
-            + '</div>'
+            + '<div class="image">' + getPictureSet(badgeInformation) + '</div>'
             + '<div class="heading">'
             + '<h4>ExtDN verified</h4>'
             + '<h3>' + badgeInformation.module_name + '</h3>'
             + '</div>'
-            + '<div class="content">'
-            + '<span title="The composer key of this extension package"><code>' + badgeInformation.composer_name + '</code></span><br/>'
-            + '<span title="The level at which there are no warnings from the Magento Coding Standard. Level 10 means that the extension is not accepted on the Magento Marketplace.">Warning-free level: ' + (badgeInformation.checks.coding_standard.warnings_at_level + 1) + '</span><br/>'
-            + '<span title="If a GraphQL endpoint exists, this could be used within PWA technologies.">GraphQL endpoint: ' + getYesOrNo(badgeInformation.checks.graphql_endpoint) + '</span><br/>'
-            + 'Unit tests: ' + getYesOrNo(badgeInformation.checks.test_unit) + '<br/>'
-            + 'Integration tests: ' + getYesOrNo(badgeInformation.checks.test_integration) + '<br/>'
-            + 'MFTF tests: ' + getYesOrNo(badgeInformation.checks.test_mftf) + '<br/>'
-            + '</div>'
+            + '<div class="content">' + getChecksContent(badgeInformation) + '</div>'
             + '</div>';
 
         badgeCallback(badgeHtml);
+
+        function getYesOrNo(value) {
+            return (value === true ? '<span class="yes">Yes</span>' : '<span class="no">No</span>');
+        }
+
+        function getPictureSet(badgeInformation) {
+            return '<picture>'
+                + '<source srcset="' + badgeInformation.image_webp + '" type="image/webp">'
+                + '<source srcset="' + badgeInformation.image_png + '" type="image/png">'
+                + '<img alt="' + badgeInformation.module_name + '" title="' + badgeInformation.module_name + '" src="' + badgeInformation.image_png + '">'
+                + '</picture>';
+        }
+
+        function getChecksContent(badgeInformation) {
+            var html = '';
+
+            function printLine(name, value, hint) {
+                if (name) html += '<span class="name" title="' + hint + '">' + name + '</span>: ';
+                html += '<span class="value" title="' + hint + '">' + value + '</span><br/>'
+            }
+
+            printLine('', '<div class="stars">Rating ' + badgeInformation.checks.coding_standard.stars + '</div>', 'A simple rating calculated from the PHPCS warning level');
+            printLine('Composer', badgeInformation.composer_name, 'The composer key of this extension package');
+
+            printLine('GraphQL endpoint', getYesOrNo(badgeInformation.checks.graphql_endpoint));
+            printLine('Marketplace compliant', getYesOrNo(badgeInformation.checks.coding_standard.warnings_at_level < 10), 'Quick check to see if this extension is compliant to Magento Marketplace coding rules');
+            printLine('PHPCS Warning Level', badgeInformation.checks.coding_standard.warnings_at_level, 'The level at which there are warnings from the Magento Coding Standard. Level 10 means that the extension is not accepted on the Magento Marketplace.');
+
+            if (badgeInformation.checks.graphql_endpoint > 0) {
+                printLine('GraphQL endpoint', getYesOrNo(badgeInformation.checks.graphql_endpoint));
+            }
+
+            printLine('Unit tests', getYesOrNo(badgeInformation.checks.test_unit));
+            printLine('Integration tests', getYesOrNo(badgeInformation.checks.test_integration));
+            printLine('MFTF tests', getYesOrNo(badgeInformation.checks.test_mftf));
+            printLine('Number of files', badgeInformation.checks.coding_standard.number_of_files);
+
+            return html;
+        }
     })
 };
 
